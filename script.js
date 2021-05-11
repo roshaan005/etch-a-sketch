8411bc98c77f0e13d0993c0e44115d9637e5f34f
anxious gritt <roshanjalil609@gmail.com>
2021-05-11 11:01:13 +0500

    fixed some bugs

---
+++ script.js
@@ -0,0 +1,158 @@
+
+// selectors 
+let container = document.getElementById("container")
+let sec = document.querySelector("section")
+let btn = document.getElementById("box")
+let slider = document.getElementById("slider")
+// grid lines visibility 
+
+btn.addEventListener("click",showGridLines)
+function showGridLines(){
+    if (btn.checked == true ){
+        container.style.gap = "1px";
+
+    }
+    else {
+        container.style.gap = "0px";
+    }
+    
+}
+// resizing grid 
+function gridSizeUserDefined(e) {
+    let gridValue = e.target.valueAsNumber;
+    gridSetDimensions(gridValue);
+    
+}
+gridSetDimensions(16);
+
+
+slider.addEventListener("change", gridSizeUserDefined)
+
+
+
+function gridSetDimensions(gridValue) {
+    container.textContent = '';
+    for (i=0; i < (gridValue * gridValue); i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('grid-el');
+        container.appendChild(pixel);
+        container.style.gridTemplateColumns = `repeat(${gridValue},auto)`
+    }
+}
+
+
+// color select buttons 
+
+sec.addEventListener("click",colorSelect)
+let colorPicker = sec.childNodes[1]
+
+function colorSelect(e){
+   
+   if (e.target.classList.contains("black")){
+    div = document.getElementsByClassName("grid-el")
+    boxes = container.querySelectorAll(".grid-el")
+    boxes.forEach(div=> {
+    div.addEventListener("mouseenter", color)
+    function color(e){
+        e.target.style.backgroundColor = "black"
+    }
+    
+    });
+    }
+    else if (e.target.classList.contains("rainbow")){
+        div = document.getElementsByClassName("grid-el")
+        boxes = container.querySelectorAll(".grid-el")
+    
+            boxes.forEach(div=> {
+                const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+                const r = randomBetween(0, 255);
+                const g = randomBetween(0, 255);
+                const b = randomBetween(0, 255);
+                const rgb = `rgb(${r},${g},${b})`; 
+        
+        
+                div.addEventListener("mouseenter", rainbowColor)
+                function rainbowColor(e){
+                e.target.style.backgroundColor = `rgb(${r},${g},${b})`
+            }
+            
+        });
+    }
+        else if(e.target.classList.contains("eraser")){
+            div = document.getElementsByClassName("grid-el")
+            boxes = container.querySelectorAll(".grid-el")
+    
+            boxes.forEach(div=> {
+                div.addEventListener("mouseenter", erase)
+                beck = div.style.backgroundColor
+                function erase(e){
+                    e.target.style.backgroundColor = beck
+
+                }
+            });
+        }else if(e.target.classList.contains("background")){
+            e.target.addEventListener("input",backColor)
+            function backColor(e){
+                let back = e.target.value;
+                div = document.getElementsByClassName("grid-el")
+                boxes = container.querySelectorAll(".grid-el")
+                    boxes.forEach(div=> {
+                       div.style.backgroundColor = back 
+
+                    
+                });
+
+            
+            
+            
+            
+            }
+        }
+
+
+}
+
+  // custom color select 
+let panel = sec.childNodes
+panelChildren = panel[1].children
+colorPick = panelChildren[5]
+background = panelChildren[6]
+clear = panelChildren[3]
+
+
+colorPick.addEventListener("input", colorOfChoice)
+
+function colorOfChoice(e){
+    let choice = e.target.value
+    
+    div = document.getElementsByClassName("grid-el")
+    boxes = container.querySelectorAll(".grid-el")
+        boxes.forEach(div=> {
+            div.addEventListener("mouseenter", choosenColor)
+            function choosenColor(e){
+                div.style.backgroundColor = choice 
+            }
+        
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
